0b5912bd4f69ba5e60e9afe683e56d1115a8247c
sabic.amra <sabic.amra@gmail.com>
2019-05-08 22:42:05 +0200

    Updates

---
+++ precache-manifest.733add4af9987562e7e7bf4f2eff463f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029a9cec463c0c03f4bcc0dec7f36f25",
+    "url": "/basic-app/index.html"
+  },
+  {
+    "revision": "fd5e3cf285bf81c4d49b",
+    "url": "/basic-app/static/css/main.38b50cdb.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/basic-app/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "fd5e3cf285bf81c4d49b",
+    "url": "/basic-app/static/js/main.0d0265d8.chunk.js"
+  },
+  {
+    "revision": "eeb5f5e66498b6977957",
+    "url": "/basic-app/static/js/runtime~main.13b124f7.js"
+  }
+]);
